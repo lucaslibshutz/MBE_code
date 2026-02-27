ca83bc1e54f2d5641fbf22d7e7d559bb89df68ed
Lucas Libshutz <lsl94@cornell.edu>
2026-02-27 01:21:09 -0500

    pr5 done

--- hw2/beaconNLS.py
+++ hw2/beaconNLS.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def beaconNLS(z, R, x0, xS):
+def beaconNLS(z, R, x0, xS,xTrue=None):
     """
     z = h(x) + v
     where z = [RA, RB, RC], and each is a distance to a becon.
@@ -15,6 +15,8 @@ def beaconNLS(z, R, x0, xS):
     Initial parameter estimate
     xS : ndarray (m,2)
     Beacon locations, each row is [x_i, y_i]
+    xTrue: ndarray (2,), optional
+    True position, used for perfect linearization of Jacobian if included 
 
     Returns
     -------
@@ -45,47 +47,71 @@ def beaconNLS(z, R, x0, xS):
     J_old = 1.0
     iter_count = 0
 
-    for _ in range(max_iter):
-
-        iter_count += 1
-        xi = xhat[:,-1].reshape(-1,1)
-
-        # Vectorize dx/dy/rhat
-        dx = xi[0,0] - xS[:,0] # shape (m,)
-        dy = xi[1,0] - xS[:,1] # shape (m,)
+    if xTrue is not None:
+        x_lin = np.asarray(xTrue).reshape(2,)
+        dx = x_lin[0] - xS[:,0] # shape (m,)
+        dy = x_lin[1] - xS[:,1] # shape (m,)
         rhat = np.sqrt(dx**2 + dy**2) # shape (m,)
         
         zhat = rhat.reshape(m,1)
-
-        # Jacobian
         Hhat = np.column_stack((dx/rhat, dy/rhat)) # shape (m,2)
 
-        M1 = np.zeros((2,2))
-        M2 = np.zeros((2,1))
+        # Precompute Rinv and the constant normal matrix
+        M1_const = n * (Hhat.T @ Rinv @ Hhat)
+        
+        sum_z = np.sum(z,axis=1).reshape(m,1)
+        sum_e = sum_z - n * zhat
+        M2_const = Hhat.T @ Rinv @ sum_e
 
-        J = 0.0
+        # Solve once for batched constant H matrix
+        dxhat = np.linalg.inv(M1_const) @ M2_const
+        xhatF = x_lin + dxhat.flatten()
+        PxhatF = np.linalg.inv(M1_const)
 
-        for k in range(n):
-            ek = z[:, k:k+1] - zhat
-            M1 += Hhat.T @ Rinv @ Hhat
-            M2 += Hhat.T @ Rinv @ ek
-            J += 0.5 * ek.T @ Rinv @ ek
+        return xhatF, PxhatF, Hhat
+
+    else:
+        for _ in range(max_iter):
+
+            iter_count += 1
+            xi = xhat[:,-1].reshape(-1,1)
+
+            # Vectorize dx/dy/rhat
+            dx = xi[0,0] - xS[:,0] # shape (m,)
+            dy = xi[1,0] - xS[:,1] # shape (m,)
+            rhat = np.sqrt(dx**2 + dy**2) # shape (m,)
+            
+            zhat = rhat.reshape(m,1)
+
+            # Jacobian
+            Hhat = np.column_stack((dx/rhat, dy/rhat)) # shape (m,2)
+
+            M1 = np.zeros((2,2))
+            M2 = np.zeros((2,1))
+
+            J = 0.0
+
+            for k in range(n):
+                ek = z[:, k:k+1] - zhat
+                M1 += Hhat.T @ Rinv @ Hhat
+                M2 += Hhat.T @ Rinv @ ek
+                J += 0.5 * ek.T @ Rinv @ ek
 
 
-        # Try except in case solution is ill-conditioned
-        try:
-            dxhat = np.linalg.inv(M1) @ M2
-        except np.linalg.LinAlgError:
-            dxhat = np.linalg.pinv(M1) @ M2
+            # Try except in case solution is ill-conditioned
+            try:
+                dxhat = np.linalg.inv(M1) @ M2
+            except np.linalg.LinAlgError:
+                dxhat = np.linalg.pinv(M1) @ M2
 
-        x_next = xi + dxhat
-        xhat = np.hstack((xhat, x_next))
+            x_next = xi + dxhat
+            xhat = np.hstack((xhat, x_next))
 
-        if abs(J_old - J.item()) < tol:
-            break
-        J_old = J.item()
+            if abs(J_old - J.item()) < tol:
+                break
+            J_old = J.item()
 
-    PxhatF = np.linalg.inv(M1)
-    xhatF = xhat[:, -1]
+        PxhatF = np.linalg.inv(M1)
+        xhatF = xhat[:, -1]
 
-    return xhatF, PxhatF, Hhat, iter_count
+        return xhatF, PxhatF, Hhat, iter_count
--- hw2/hw2_problem5.py
+++ hw2/hw2_problem5.py
@@ -45,13 +45,32 @@ print(f"Number of iterations: {iter_countA}")
 ii = [0,1]
 R_b1 = R_a[np.ix_(ii,ii)]
 z_b1 = z_a[ii,:]
+#NOTE: Compute B and C together to avid defining vars ii, z_bi, R_bi twice
+
+xhatB1, PxhatB1, HhatB1, iter_countB1 = beaconNLS(z_b1, R_b1, x0, xS[ii,:])
+xhatC1, PxhatC1, HhatC1 = beaconNLS(z_b1, R_b1, x0, xS[ii,:],xtrue.flatten())
+print("\nProblem 5(b): Two beacons (A,B)")
+print(f"xHat = {xhatB1}")
+print(f"Pxhat = {PxhatB1}")
 
 # (B,C)
 ii = [1,2]
 R_b2 = R_a[np.ix_(ii,ii)]
 z_b2 = z_a[ii,:]
 
+xhatB2, PxhatB2, HhatB2, iter_countB2 = beaconNLS(z_b2, R_b2, x0, xS[ii,:])
+xhatC2, PxhatC2, HhatC2 = beaconNLS(z_b2, R_b2, x0, xS[ii,:],xtrue.flatten())
+print("\nProblem 5(b): Two beacons (B,C)")
+print(f"xHat = {xhatB2}")
+print(f"Pxhat = {PxhatB2}")
+
 # Part (c): Perfect linearization
+# Repeat two cases from (b) but with x0=xTrue
+
+
+print("\nProblem 5(c): Perfect linearization")
+print(f"(A,B) with x0=xTrue: xHat = {xhatC1}, Pxhat = {PxhatC1}")
+print(f"(B,C) with x0=xTrue: xHat = {xhatC2}, Pxhat = {PxhatC2}")
 
 # Part (d): Correlated noise
 R_d = np.array([
@@ -62,3 +81,7 @@ R_d = np.array([
 v_d = np.linalg.cholesky(R_d) @ np.random.randn(3,n)
 z_d = np.tile(np.array([RA, RB, RC]).reshape(-1,1), (1,n)) + v_d
 
+xhatD, PxhatD, HhatD, iter_countD = beaconNLS(z_d, R_d, x0, xS)
+print("\nProblem 5(d): Correlated Noise")
+print(f"xHat = {xhatD}")
+print(f"PxHat = {PxhatD}")
