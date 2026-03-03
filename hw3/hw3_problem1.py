f19b277e57df6cbab9d3f9b4ad5105604aa06d28
Lucas Libshutz <lsl94@cornell.edu>
2026-03-02 22:37:54 -0500

    hw3 first things

---
+++ hw3/hw3_problem1.py
@@ -0,0 +1,106 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.signal import cont2discrete
+from scipy.linalg import solve_discrete_are
+
+from plot_openloop import plot_openloop
+from plot_estimator import plot_estimator
+
+# Define CT system
+k1 = k2 = k3 = k4 = k5 = 1
+c1 = c2 = c3 = c4 = c5 = 0.01
+m1 = m2 = m3 = m4 = m5 = 1
+
+nx = 10
+Fc = np.zeros((nx,nx))
+
+# stiffness
+Fc[np.ix_([1,3,5,7,9],[0,2,4,6,8])] = np.array([
+[-(k1+k2)/m1, (k2)/m1, 0, 0, 0],
+[ (k2)/m2, -(k2+k3)/m2, (k3)/m2, 0, 0],
+[ 0, (k3)/m3, -(k3+k4)/m3, (k4)/m3, 0],
+[ 0, 0, (k4)/m4, -(k4+k5)/m4, (k5)/m4],
+[ 0, 0, 0, (k5)/m5, -(k5)/m5]
+])
+# damping
+Fc[np.ix_([1,3,5,7,9],[1,3,5,7,9])] = np.array([
+[-(c1+c2)/m1, (c2)/m1, 0, 0, 0],
+[ (c2)/m2, -(c2+c3)/m2, (c3)/m2, 0, 0],
+[0,(c3)/m3, -(c3+c4)/m3,(c4)/m3,0],
+[0,0,(c4)/m4, -(c4+c5)/m4,(c5)/m4],
+[0,0,0,(c5)/m5, -(c5)/m5]
+])
+# kinematics
+Fc[np.ix_([0,2,4,6,8],[1,3,5,7,9])] = np.eye(5)
+
+# disturbance on fifth mass
+Gc = np.zeros((nx,1))
+Gc[9,0] = 1/m5
+
+# position measurements on 1st and 5th measurements
+i1 = 0
+Hq1 = np.zeros((1,nx))
+Hq1[0,i1] = 1
+i5 = 8
+Hq5 = np.zeros((1,nx))
+Hq5[0,i5] = 1
+Hc = np.vstack((Hq1,Hq5))
+
+# discrete time model
+dt = 0.05
+F, G, _, _, _ = cont2discrete((Fc, Gc, Hc, 0), dt)
+
+# Simulate system
+t = np.arange(0,50+dt,dt)
+nk = len(t)
+
+x0 = np.zeros(nx)
+x0[8] = 1
+
+np.random.seed(101)
+Qsim = 0.01 # process noise covariance
+w = np.sqrt(Qsim) * np.random.randn(1,nk)
+
+x_true = np.zeros((nx,nk))
+x_no_w = np.zeros((nx,nk))
+x_true[:,0] = x0
+x_no_w[:,0] = x0
+
+for k in range(nk-1):
+    x_no_w[:, k+1] = F @ x_no_w[:,k]
+    x_true[:, k+1] = F @ x_true[:,k] + G @ w[:,k]
+
+# measurements
+Rq1 = 0.001
+Rq5 = 0.001
+z_q1 = (Hq1 @ x_true + np.sqrt(Rq1) * np.random.randn(1,nk)).ravel() # scalar measurement
+z_q5 = (Hq5 @ x_true + np.sqrt(Rq5) * np.random.randn(1,nk)).ravel() # scalar measurement
+
+# open loop plots
+fig, axs = plt.subplots(1,2, figsize=(16,6))
+
+plot_openloop(t, x_true[i1, :], z=z_q1, x_no_w=x_no_w[i1,:],ax=axs[0])
+axs[0].set_ylabel("position of first mass")
+
+plot_openloop(t, x_true[i5, :], z=z_q5, x_no_w=x_no_w[i5,:],ax=axs[1])
+axs[1].set_ylabel("position of fifth mass")
+
+fig.suptitle("Siimulated Response: 1st and 5th Masses",fontweight='bold')
+
+plt.tight_layout()
+
+### --------------------------------------
+### (a) KF using z_q1
+### --------------------------------------
+
+### --------------------------------------
+### (b) Predicted vs Updated Covariances
+### --------------------------------------
+
+### --------------------------------------
+### (c) Steady State Error Covariance
+### --------------------------------------
+
+### --------------------------------------
+### (d) Constant gain KF
+### --------------------------------------
