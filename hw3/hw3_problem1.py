ce55b7a56238d6cf4d17bcf7b63b2f9a0324e775
Lucas Libshutz <lsl94@cornell.edu>
2026-03-09 13:40:15 -0400

    first version of working KF for pr1

--- hw3/hw3_problem1.py
+++ hw3/hw3_problem1.py
@@ -88,11 +88,44 @@ axs[1].set_ylabel("position of fifth mass")
 fig.suptitle("Siimulated Response: 1st and 5th Masses",fontweight='bold')
 
 plt.tight_layout()
+plt.show()
 
 ### --------------------------------------
 ### (a) KF using z_q1
-### --------------------------------------
 
+# Initialize variables
+xhatA = np.zeros((nx,nk))
+PxhatA = np.zeros((nx,nx,nk))
+xhatA[:,0] = np.zeros(nx) # initial state estimate
+PxhatA[:,:,0] = np.eye(nx) # initial error covariance
+Q = np.atleast_2d(Qsim) # process noise covariance
+# stack measurements into single array
+zA = np.vstack((z_q1,z_q5))
+
+for k in range(nk-1):
+    # Prediction step
+    xPred = F @ xhatA[:,k]
+    PxPred = F @ PxhatA[:,:,k] @ F.T + G @ Q @ G.T
+
+    # Kalman Gain
+    K_ins = Hc @ PxPred @ Hc.T + np.diag([Rq1, Rq5])
+    K = (PxPred @ Hc.T)@ np.linalg.inv(K_ins)
+
+    # Update step
+    xhatA[:,k+1] = xPred + K @ (zA[:,k+1]- Hc@ xPred)
+    # PxhatA[:, :, k+1] = (np.eye(nx) - K @ Hc) @ PxPred
+    origTerm = np.eye(nx) - K @ Hc
+    PxhatA[:, :, k+1] = origTerm @ PxPred @ origTerm.T + K @ np.diag([Rq1, Rq5]) @ K.T
+
+
+# Plot results
+fig, axs = plt.subplots(1,2, figsize=(16,6))
+# plot_openloop(t, x_true[i1, :], z=z_q1, x_no_w=x_no_w[i1,:],ax=axs[0])
+plot_estimator(t, xhatA[i1,:], PxhatA[i1,i1,:],x_true[i1,:],plot_type='state',ax=axs[0])
+# plot_openloop(t, x_true[i5, :], z=z_q5, x_no_w=x_no_w[i5,:],ax=axs[1])
+plot_estimator(t, xhatA[i5,:], PxhatA[i5,i5,:],x_true[i5,:],plot_type='state',ax=axs[1])
+fig.tight_layout()
+plt.show()
 ### --------------------------------------
 ### (b) Predicted vs Updated Covariances
 ### --------------------------------------
