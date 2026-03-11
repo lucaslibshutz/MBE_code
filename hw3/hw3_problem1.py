40fb430ac3f4df7869a13ad70133269dceda1f8e
Lucas Libshutz <lsl94@cornell.edu>
2026-03-11 13:38:07 -0400

    fix files

--- hw3/hw3_problem1.py
+++ hw3/hw3_problem1.py
@@ -3,14 +3,15 @@ from matplotlib import pyplot as plt
 from scipy.signal import cont2discrete
 from scipy.linalg import solve_discrete_are
 import addcopyfighandler
+from kalmanFilter import kalman_filter
 
 from plot_openloop import plot_openloop
 from plot_estimator import plot_estimator
 
 # environment vars
 plot_I = 0
-plot_A = 0
-plot_D = 1
+plot_A = 1
+plot_D = 0
 
 # Define CT system
 k1 = k2 = k3 = k4 = k5 = 1
@@ -99,75 +100,76 @@ if plot_I:
 
 ### --------------------------------------
 ### (a) KF using z_q1
+# KF for each mass, and see the uncertainty in the other
+xhat_pA1, xhat_uA1, Pxhat_pA1, Pxhat_uA1 = kalman_filter(
+    F=F,
+    G=G,
+    H=Hq1,
+    Q=Qsim,
+    R=np.diag([Rq1]),
+    z=z_q1,
+    x0=x0,
+    P0=np.eye(nx),
+    nk=nk
+)
 
-# Initialize variables
-xhat_pA = np.zeros((nx,nk))
-xhat_uA = np.zeros((nx,nk))
-xhat_pA[:,0] = x0 # initial state estimate
-xhat_uA[:,0] = x0 # initial state estimate
-# xhat_pA[:,0] = np.zeros(nx) # initial state estimate
-# xhat_uA[:,0] = np.zeros(nx) # initial state estimate
-Pxhat_pA = np.zeros((nx,nx,nk))
-Pxhat_uA = np.zeros((nx,nx,nk))
-Pxhat_pA[:,:,0] = np.eye(nx,nx) # initial error covariance
-Pxhat_uA[:,:,0] = np.eye(nx,nx) # initial error covariance
-Q = np.atleast_2d(Qsim) # process noise covariance
-# stack measurements into single array
-zA = np.vstack((z_q1,z_q5))
-# zA = z_q1
-
-#WARNING: Two at once: is this fine?
-for k in range(nk-1):
-    # Prediction step
-    xhat_pA[:,k+1] = F @ xhat_uA[:,k]
-    Pxhat_pA[:,:,k+1] = F @ Pxhat_uA[:,:,k] @ F.T + G @ Q @ G.T
-
-    xPred = xhat_pA[:,k+1]
-    PxPred = Pxhat_pA[:,:,k+1]
-
-    # Kalman Gain
-    K_ins = Hc @ PxPred @ Hc.T + np.diag([Rq1, Rq5]) # both masses
-    # K_ins = Hq1 @ PxPred @ Hq1.T + Rq1
-    K = (PxPred @ Hc.T)@ np.linalg.inv(K_ins) # both masses
-    # K = (PxPred @ Hq1.T)@ np.linalg.inv(K_ins)
-
-    # Update step
-    xhat_uA[:,k+1] = xPred + K @ (zA[:,k+1]- Hc@ xPred) # both masses
-    # xhat_uA[:,k+1] = xPred + K @ (zA[k+1]- Hq1@ xPred)
-    origTerm = np.eye(nx) - K @ Hc # both masses
-    # origTerm = np.eye(nx) - K @ Hq1
-    Pxhat_uA[:, :, k+1] = origTerm @ PxPred @ origTerm.T + K @ np.diag([Rq1, Rq5]) @ K.T # both masses
-    # Pxhat_uA[:, :, k+1] = origTerm @ PxPred @ origTerm.T + K @ np.atleast_2d(Rq1) @ K.T
+xhat_pA5, xhat_uA5, Pxhat_pA5, Pxhat_uA5 = kalman_filter(
+    F=F,
+    G=G,
+    H=Hq5,
+    Q=Qsim,
+    R=np.diag([Rq5]),
+    z=z_q5,
+    x0=x0,
+    P0=np.eye(nx),
+    nk=nk
+)
 
 if plot_A:
     # Plot results
-    fig, axs = plt.subplots(1,2, figsize=(16,6))
 
+    ## -- First mass KF --
+    fig, axs = plt.subplots(1,2, figsize=(16,6))
     # First mass
-    # axs[0].plot(t[::2], z_q1[::2], 'g.', label='Measurements')
-    plot_estimator(t, xhat_uA[i1,:], Pxhat_uA[i1,i1,:],x_true[i1,:],plot_type='state',z=z_q1,ax=axs[0])
+    plot_estimator(t, xhat_uA1[i1,:], Pxhat_uA1[i1,i1,:],x_true[i1,:],plot_type='state',z=z_q1,ax=axs[0])
     axs[0].set_ylabel(r"position of first mass $m_1$")
 
     # Fifth mass
-    # axs[1].plot(t[::2], z_q5[::2], 'g.', label='Measurements')
-    plot_estimator(t, xhat_uA[i5,:], Pxhat_uA[i5,i5,:],x_true[i5,:],plot_type='state',z=z_q5,ax=axs[1])
+    plot_estimator(t, xhat_uA1[i5,:], Pxhat_uA1[i5,i5,:],x_true[i5,:],plot_type='state',z=None,ax=axs[1])
     axs[1].set_ylabel(r"position of fifth mass $m_5$")
-    fig.tight_layout()
+    fig.suptitle("First mass KF",fontweight='bold')
+    plt.tight_layout()
     plt.show()
+
+    ## -- Fifth mass KF --
+    fig, axs = plt.subplots(1,2, figsize=(16,6))
+    # First mass
+    plot_estimator(t, xhat_uA5[i1,:], Pxhat_uA5[i1,i1,:],x_true[i1,:],plot_type='state',z=None,ax=axs[0])
+    axs[0].set_ylabel(r"position of first mass $m_1$")
+
+    # Fifth mass
+    plot_estimator(t, xhat_uA5[i5,:], Pxhat_uA5[i5,i5,:],x_true[i5,:],plot_type='state',z=z_q5,ax=axs[1])
+    axs[1].set_ylabel(r"position of fifth mass $m_5$")
+    fig.suptitle("Fifth mass KF",fontweight='bold')
+    plt.tight_layout()
+    plt.show()
+
+
+
     ### --------------------------------------
-    ### (b) Predicted vs Updated Covariances for mass 5
+    ### (b) Predicted vs Updated Covariances for mass 1 & 5
     ### --------------------------------------
     fig,axs = plt.subplots(1,2, figsize=(16,6))
-    plot_estimator(t, xhat_pA[i1,:], Pxhat_pA[i1,i1,:],x_true[i1,:],plot_type='error',ax=axs[0])
-    plot_estimator(t, xhat_uA[i1,:], Pxhat_uA[i1,i1,:],x_true[i1,:],plot_type='error',z=z_q1,ax=axs[1])
+    plot_estimator(t, xhat_pA1[i1,:], Pxhat_pA1[i1,i1,:],x_true[i1,:],plot_type='error',ax=axs[0])
+    plot_estimator(t, xhat_uA1[i1,:], Pxhat_uA1[i1,i1,:],x_true[i1,:],plot_type='error',z=z_q1,ax=axs[1])
     axs[0].set_title(r"Predicted Error $(q_1)$")
     axs[1].set_title(r"Updated Error $(q_1)$")
     fig.tight_layout()
     plt.show()
 
     fig,axs = plt.subplots(1,2, figsize=(16,6))
-    plot_estimator(t, xhat_pA[i5,:], Pxhat_pA[i5,i5,:],x_true[i5,:],plot_type='error',ax=axs[0])
-    plot_estimator(t, xhat_uA[i5,:], Pxhat_uA[i5,i5,:],x_true[i5,:],plot_type='error',z=z_q5,ax=axs[1])
+    plot_estimator(t, xhat_pA1[i5,:], Pxhat_pA1[i5,i5,:],x_true[i5,:],plot_type='error',ax=axs[0])
+    plot_estimator(t, xhat_uA1[i5,:], Pxhat_uA1[i5,i5,:],x_true[i5,:],plot_type='error',z=None,ax=axs[1])
     axs[0].set_title(r"Predicted Error $(q_5)$")
     axs[1].set_title(r"Updated Error $(q_5)$")
     fig.tight_layout()
@@ -177,14 +179,14 @@ if plot_A:
     # Plot variances and error of fifth mass for predicted and updated
     fig, axs = plt.subplots(1,2, figsize=(16,6))
 
-    axs[0].semilogy(t, Pxhat_pA[i1,i1,:], 'r',label='Predicted')
-    axs[0].semilogy(t, Pxhat_uA[i1,i1,:], 'b',label='Updated')
+    axs[0].semilogy(t, Pxhat_pA1[i1,i1,:], 'r',label='Predicted')
+    axs[0].semilogy(t, Pxhat_uA1[i1,i1,:], 'b',label='Updated')
     axs[0].set_xlabel("Time (s)")
     axs[0].set_ylabel(r"$\sigma^2(m_1)$")
     axs[0].legend()
 
-    axs[1].semilogy(t, Pxhat_pA[i5,i5,:],'r', label='Predicted')
-    axs[1].semilogy(t, Pxhat_uA[i5,i5,:],'b', label='Updated')
+    axs[1].semilogy(t, Pxhat_pA1[i5,i5,:],'r', label='Predicted')
+    axs[1].semilogy(t, Pxhat_uA1[i5,i5,:],'b', label='Updated')
     axs[1].set_xlabel("Time (s)")
     axs[1].set_ylabel(r"$\sigma^2(m_5)$")
     axs[1].legend()
@@ -194,10 +196,15 @@ if plot_A:
 ### --------------------------------------
 ### (c) Steady State Error Covariance
 ### --------------------------------------
-Pbar = solve_discrete_are(a=F.T, b=Hc.T, q=G @ Q @ G.T, r=np.diag([Rq1, Rq5]))
+Pbar1 = solve_discrete_are(a=F.T, b=Hq1.T, q=G @ np.atleast_2d(Qsim) @ G.T, r=np.diag([Rq1]))
+Pbar5 = solve_discrete_are(a=F.T, b=Hq5.T, q=G @ np.atleast_2d(Qsim) @ G.T, r=np.diag([Rq5]))
 
-print(f"Simulated measurement covariance at the final time is: {Pxhat_uA[i5,i5,nk-1]}")
-print(f"Steady state error covariance is: {Hq5 @ Pbar @ Hq5.T}")
+print(f"Simulated measurement covariance for mass 1 at the final time is: {(Pxhat_uA1[i1,i1,nk-1]).astype(float)}")
+print(f"Steady state error covariance for mass 1 is: {(Hq1 @ Pbar1 @ Hq1.T).astype(float)}")
+
+print(f"Simulated measurement covariance for mass 5 at the final time is: {(Pxhat_uA5[i5,i5,nk-1]).astype(float)}")
+print(f"Steady state error covariance for mass 5 is: {(Hq5 @ Pbar5 @ Hq5.T).astype(float)}")
+
 
 
 ### --------------------------------------
@@ -206,58 +213,58 @@ print(f"Steady state error covariance is: {Hq5 @ Pbar @ Hq5.T}")
 # Perform the same KF from (a), but this time use P0 = Pbar
 
 # Initialize variables
-xhat_pD = np.zeros((nx,nk))
-xhat_uD = np.zeros((nx,nk))
-xhat_pD[:,0] = x0 # initial state estimate
-xhat_uD[:,0] = x0 # initial state estimate
-# xhat_pD[:,0] = np.zeros(nx) # initial state estimate
-# xhat_uD[:,0] = np.zeros(nx) # initial state estimate
-Pxhat_pD = np.zeros((nx,nx,nk))
-Pxhat_uD = np.zeros((nx,nx,nk))
-Pxhat_pD[:,:,0] = Pbar # initial error covariance
-Pxhat_uD[:,:,0] = Pbar # initial error covariance
-Q = np.atleast_2d(Qsim) # process noise covariance
-# stack measurements into single array
-# zD = np.vstack((z_q1,z_q5))
-zD = z_q5
-Kbar = Pbar @ Hq5.T @ np.linalg.inv(Hq5 @ Pbar @ Hq5.T + np.diag([Rq5]))
-
-for k in range(nk-1):
-    # Prediction step
-    xhat_pD[:,k+1] = F @ xhat_uD[:,k]
-    Pxhat_pD[:,:,k+1] = F @ Pxhat_uD[:,:,k] @ F.T + G @ Q @ G.T
-
-    xPred = xhat_pD[:,k+1]
-    PxPred = Pxhat_pD[:,:,k+1]
-
-    # Kalman Gain
-    K_ins = Hq5 @ PxPred @ Hq5.T + np.diag([Rq1, Rq5])
-    # K = (PxPred @ Hc.T)@ np.linalg.inv(K_ins)
-    K = Kbar
-
-    # Update step
-    xhat_uD[:,k+1] = xPred + K @ (zD[k+1]- Hq5@ xPred)
-    # PxhatD[:, :, k+1] = (np.eye(nx) - K @ Hc) @ PxPred
-    origTerm = np.eye(nx) - K @ Hq5
-    Pxhat_uD[:, :, k+1] = origTerm @ PxPred @ origTerm.T + K @ np.diag([Rq5]) @ K.T
-
-# Plot estimator for part (D)
-if plot_D:
-    fig, axs = plt.subplots(1,2, figsize=(16,6))
-    plot_estimator(t, xhat_uD[i1,:], Pxhat_uD[i1,i1,:],x_true[i1,:],plot_type='state',z=z_q1,ax=axs[0])
-    plot_estimator(t, xhat_uD[i5,:], Pxhat_uD[i5,i5,:],x_true[i5,:],plot_type='state',z=z_q5,ax=axs[1])
-    axs[0].set_title("Steady State Covariance & Gain KF: 1st Mass")
-    axs[1].set_title("Steady State Covariance & Gain KF: 5th Mass")
-    fig.tight_layout()
-    plt.show()
-
-    fig, axs = plt.subplots(1,2, figsize=(16,6))
-    plot_estimator(t, xhat_pD[i1,:], Pxhat_pD[i1,i1,:],x_true[i1,:],plot_type='error',ax=axs[0])
-    plot_estimator(t, xhat_pD[i5,:], Pxhat_pD[i5,i5,:],x_true[i5,:],plot_type='error',z=z_q5,ax=axs[1])
-    axs[0].set_ylim([-0.1,0.1])
-    axs[1].set_ylim([-0.2,0.2])
-    axs[0].set_title("Steady State Covariance & Gain KF: 1st Mass Error")
-    axs[1].set_title("Steady State Covariance & Gain KF: 5th Mass Error")
-    fig.tight_layout()
-    plt.show()
+# xhat_pD = np.zeros((nx,nk))
+# xhat_uD = np.zeros((nx,nk))
+# xhat_pD[:,0] = x0 # initial state estimate
+# xhat_uD[:,0] = x0 # initial state estimate
+# # xhat_pD[:,0] = np.zeros(nx) # initial state estimate
+# # xhat_uD[:,0] = np.zeros(nx) # initial state estimate
+# Pxhat_pD = np.zeros((nx,nx,nk))
+# Pxhat_uD = np.zeros((nx,nx,nk))
+# Pxhat_pD[:,:,0] = Pbar # initial error covariance
+# Pxhat_uD[:,:,0] = Pbar # initial error covariance
+# Q = np.atleast_2d(Qsim) # process noise covariance
+# # stack measurements into single array
+# # zD = np.vstack((z_q1,z_q5))
+# zD = z_q5
+# Kbar = Pbar @ Hq5.T @ np.linalg.inv(Hq5 @ Pbar @ Hq5.T + np.diag([Rq5]))
+#
+# for k in range(nk-1):
+#     # Prediction step
+#     xhat_pD[:,k+1] = F @ xhat_uD[:,k]
+#     Pxhat_pD[:,:,k+1] = F @ Pxhat_uD[:,:,k] @ F.T + G @ Q @ G.T
+#
+#     xPred = xhat_pD[:,k+1]
+#     PxPred = Pxhat_pD[:,:,k+1]
+#
+#     # Kalman Gain
+#     K_ins = Hq5 @ PxPred @ Hq5.T + np.diag([Rq1, Rq5])
+#     # K = (PxPred @ Hc.T)@ np.linalg.inv(K_ins)
+#     K = Kbar
+#
+#     # Update step
+#     xhat_uD[:,k+1] = xPred + K @ (zD[k+1]- Hq5@ xPred)
+#     # PxhatD[:, :, k+1] = (np.eye(nx) - K @ Hc) @ PxPred
+#     origTerm = np.eye(nx) - K @ Hq5
+#     Pxhat_uD[:, :, k+1] = origTerm @ PxPred @ origTerm.T + K @ np.diag([Rq5]) @ K.T
+#
+# # Plot estimator for part (D)
+# if plot_D:
+#     fig, axs = plt.subplots(1,2, figsize=(16,6))
+#     plot_estimator(t, xhat_uD[i1,:], Pxhat_uD[i1,i1,:],x_true[i1,:],plot_type='state',z=z_q1,ax=axs[0])
+#     plot_estimator(t, xhat_uD[i5,:], Pxhat_uD[i5,i5,:],x_true[i5,:],plot_type='state',z=z_q5,ax=axs[1])
+#     axs[0].set_title("Steady State Covariance & Gain KF: 1st Mass")
+#     axs[1].set_title("Steady State Covariance & Gain KF: 5th Mass")
+#     fig.tight_layout()
+#     plt.show()
+#
+#     fig, axs = plt.subplots(1,2, figsize=(16,6))
+#     plot_estimator(t, xhat_pD[i1,:], Pxhat_pD[i1,i1,:],x_true[i1,:],plot_type='error',ax=axs[0])
+#     plot_estimator(t, xhat_pD[i5,:], Pxhat_pD[i5,i5,:],x_true[i5,:],plot_type='error',z=z_q5,ax=axs[1])
+#     axs[0].set_ylim([-0.1,0.1])
+#     axs[1].set_ylim([-0.2,0.2])
+#     axs[0].set_title("Steady State Covariance & Gain KF: 1st Mass Error")
+#     axs[1].set_title("Steady State Covariance & Gain KF: 5th Mass Error")
+#     fig.tight_layout()
+#     plt.show()
 
